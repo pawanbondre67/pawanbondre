05a76853da5a176708d5b4a263ee99830ff44831
pawanbondre67 <bondrepavan623@gmail.com>
2024-10-16 20:12:49 +0530

    routing added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Projects from './components/Projects/Projects.tsx';
+import ProjectDetails from './components/Projects/ProjectDetails.tsx';
+import Header from './components/header/Header.tsx';
+import Footer from './components/footer/Footer.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />, 
+  },
+  {
+    path: "/projects/:id",
+    element: <ProjectDetails />
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+  <StrictMode>  
+    <Header />
+  <RouterProvider router={router} />
+  <Footer/>
   </StrictMode>,
 )
