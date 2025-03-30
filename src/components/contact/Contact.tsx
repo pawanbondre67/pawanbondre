7ac128516b02ea2dd01f2d925dfd6d52c36f5356
pawanbondre67 <pawanbondre19@gmail.com>
2025-03-30 15:10:10 +0530

    added mail service

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,36 +1,76 @@
-import React, { useState } from 'react';
-import { useToast } from "@/hooks/use-toast";
+import React, { useState } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
 
 const Contact: React.FC = () => {
-  const { toast } = useToast();
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
+    name: "",
+    email: "",
+    message: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const { name, email, message } = formData;
+    if (!name || !email || !message) {
+      toast("Please fill in all fields");
+      return;
+    }
+    setIsLoading(true);
 
-    if (name && email && message) {
-      toast({
-        description: "Your message has been sent.",
-      });
-      // Optionally, clear the form
-      setFormData({ name: '', email: '', message: '' });
-    } else {
-      toast({
-        description: "Please fill in all fields.",
-      });
+    const detailsData = {
+      service_id: import.meta.env.VITE_PUBLIC_EMAILJS_SERVICE_ID,
+      template_id: import.meta.env.VITE_PUBLIC_EMAILJS_TEMPLATE_ID,
+      user_id: import.meta.env.VITE_PUBLIC_EMAILJS_USER_ID,
+      template_params: {
+        name: formData.name,
+        email: formData.email,
+      },
+    };
+
+    console.log(detailsData);
+
+    try {
+      const response = await fetch(
+        "https://api.emailjs.com/api/v1.0/email/send",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(detailsData),
+        }
+      );
+
+      if (response.ok) {
+        toast.success("Thanks for reaching out! I'll get back to you soon.");
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        });
+      } else {
+        const errorData = await response.json();
+        console.log(errorData);
+        toast("Failed to send mail. Please try again.");
+      }
+    } catch (error) {
+      console.log("Error:", error);
+      toast("An error occurred: " + error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -49,29 +89,133 @@ const Contact: React.FC = () => {
             </div>
             <div className="space-y-2">
               <div className="flex items-center gap-2">
-                <svg className="h-5 w-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline><path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path></svg>
-                <span className="text-gray-500 dark:text-gray-400">pawanbondre19@gmail.com</span>
+                <svg
+                  className="h-5 w-5 text-gray-500 dark:text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
+                  <path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
+                </svg>
+                <span className="text-gray-500 dark:text-gray-400">
+                  pawanbondre19@gmail.com
+                </span>
               </div>
               <div className="flex items-center gap-2">
-                <svg className="h-5 w-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
-                <span className="text-gray-500 dark:text-gray-400">+91-817780XXXX</span>
+                <svg
+                  className="h-5 w-5 text-gray-500 dark:text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                </svg>
+                <span className="text-gray-500 dark:text-gray-400">
+                  +91-817780XXXX
+                </span>
               </div>
               <div className="flex items-center gap-2">
-                <svg className="h-5 w-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 10.5C21 16.29 12 21 12 21s-9-4.71-9-10.5a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10.5" r="3"></circle></svg>
-                <span className="text-gray-500 dark:text-gray-400">Somewhere on Earth</span>
+                <svg
+                  className="h-5 w-5 text-gray-500 dark:text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M21 10.5C21 16.29 12 21 12 21s-9-4.71-9-10.5a9 9 0 0 1 18 0z"></path>
+                  <circle cx="12" cy="10.5" r="3"></circle>
+                </svg>
+                <span className="text-gray-500 dark:text-gray-400">
+                  Somewhere on Earth
+                </span>
               </div>
               <div>
                 <div className="flex items-center mt-4 gap-4">
-                  <a target="_blank" className="text-[#0077B5] hover:bg-[#0077B5] bg-background bg-gray-950 dark:text-gray-50 rounded-full p-2 hover:text-background transition-colors" data-state="closed" href="https://linkedin.com/in/pawan-bondre-6262b1243">
-                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+                  <a
+                    target="_blank"
+                    className="text-[#0077B5] hover:bg-[#0077B5] bg-background bg-gray-950 dark:text-gray-50 rounded-full p-2 hover:text-background transition-colors"
+                    data-state="closed"
+                    href="https://linkedin.com/in/pawan-bondre-6262b1243"
+                  >
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                      <rect width="4" height="12" x="2" y="9"></rect>
+                      <circle cx="4" cy="4" r="2"></circle>
+                    </svg>
                     <span className="sr-only">LinkedIn</span>
                   </a>
-                  <a target="_blank" className="text-[#1DA1F2] hover:bg-[#1DA1F2] bg-background bg-gray-950 dark:text-gray-50 rounded-full p-2 hover:text-background transition-colors" data-state="closed" href="https://twitter.com/pawancodes">
-                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path></svg>
+                  <a
+                    target="_blank"
+                    className="text-[#1DA1F2] hover:bg-[#1DA1F2] bg-background bg-gray-950 dark:text-gray-50 rounded-full p-2 hover:text-background transition-colors"
+                    data-state="closed"
+                    href="https://twitter.com/pawancodes"
+                  >
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
+                    </svg>
                     <span className="sr-only">Twitter</span>
                   </a>
-                  <a target="_blank" className="hover:bg-[#333] bg-background bg-gray-950 text-gray-50 rounded-full p-2 hover:text-background transition-colors" data-state="closed" href="https://github.com/pawanbondre67">
-                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
+                  <a
+                    target="_blank"
+                    className="hover:bg-[#333] bg-background bg-gray-950 text-gray-50 rounded-full p-2 hover:text-background transition-colors"
+                    data-state="closed"
+                    href="https://github.com/pawanbondre67"
+                  >
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
+                      <path d="M9 18c-4.51 2-5-2-7-2"></path>
+                    </svg>
                     <span className="sr-only">GitHub</span>
                   </a>
                 </div>
@@ -81,36 +225,43 @@ const Contact: React.FC = () => {
           <form onSubmit={handleSubmit}>
             <div className="text-white rounded-xl border bg-card bg-gray-950 border-gray-800">
               <div className="flex flex-col space-y-1.5 p-6">
-                <h3 className="font-semibold text-white leading-none tracking-tight">Contact Me</h3>
+                <h3 className="font-semibold text-white leading-none tracking-tight">
+                  Contact Me
+                </h3>
                 <p className="text-sm text-muted-foreground dark:text-gray-400">
-                  Fill out the form below and I'll get back to you as soon as possible.
+                  Fill out the form below and I'll get back to you as soon as
+                  possible.
                 </p>
               </div>
               <div className="p-6 pt-0 space-y-4">
                 <div className="grid grid-cols-2 gap-4">
                   <div className="space-y-2">
-                    <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="name">
+                    <label
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="name"
+                    >
                       Name
                     </label>
                     <input
                       className="flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 border-gray-800"
                       id="name"
                       placeholder="Enter your name"
-                      required
                       name="name"
                       value={formData.name}
                       onChange={handleChange}
                     />
                   </div>
                   <div className="space-y-2">
-                    <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="email">
+                    <label
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      htmlFor="email"
+                    >
                       Email
                     </label>
                     <input
                       className="flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 border-gray-800"
                       id="email"
                       placeholder="Enter your email"
-                      required
                       type="email"
                       name="email"
                       value={formData.email}
@@ -119,7 +270,10 @@ const Contact: React.FC = () => {
                   </div>
                 </div>
                 <div className="space-y-2">
-                  <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="message">
+                  <label
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    htmlFor="message"
+                  >
                     Message
                   </label>
                   <textarea
@@ -137,12 +291,13 @@ const Contact: React.FC = () => {
                   className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 bg-black border border-gray-600 hover:bg-gray-200 hover:text-gray-500"
                   type="submit"
                 >
-                  Submit
+                  {isLoading ? "Submitting..." : "Submit"}
                 </button>
               </div>
             </div>
           </form>
         </div>
+        <ToastContainer autoClose={2000}  draggable />
       </section>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import dotenv from 'dotenv';
+
+
+dotenv.config(); 
 
 export default defineConfig({
   plugins: [react()],
