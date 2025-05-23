2874293c1513627d7652d791c4d1e2f2acc352d3
pawanbondre67 <pawanbondre19@gmail.com>
2025-05-23 16:56:40 +0530

    frontend url added

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const editRequestRoutes = require('./routes/editRequestRoutes');
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://pawanbondre.tech', 
     credentials: true, // Allow credentials (cookies)
     optionsSuccessStatus: 200 
   };
